59506e6beef9ec921c6a4ae540d398afbe5e1ad2
Halvinn <onesih@hotmail.com>
2018-08-22 13:35:16 -0400

    Download files and save locally as csv

--- Case_Study2/emissions.py
+++ Case_Study2/emissions.py
@@ -12,23 +12,51 @@ and csv files. I downloaded the data directly from the website.
 #%% Download and Load Data
 import pandas as pd
 import numpy as np
-import urllib4
+import csv
+import requests
 
 #%%
+"""
+Download csv file and write to csv file. Only necessary 1st time to download
+file locally.
+
 url_08 = "https://www.epa.gov/sites/production/files/2016-07/08tstcar.csv"
-socket = urllib.urlopen(url_08)
-ex = pd.ExcelFile(socket)
+filename = "data_08.csv"
 
-print(ex.sheet_names)
+with open(filename, 'wb') as out_file:
+    out_file.write(requests.get(url_08).content)
+"""
 
+# Read csv file into a dataframe
+df_08 = pd.read_csv("data_08.csv")
+df_08.head()
 #%%
 url_18 = "https://www.epa.gov/sites/production/files/2018-06/18tstcar\
 -2018-05-30.xlsx"
 
+xl = pd.ExcelFile(url_18, sep="\t")
 
-df_08 = pd.read_excel(url_08)
-df_18 = pd.read_csv(url_18)
+sheet_name = xl.sheet_names[0]
 
-print(df_08.head())
+df_18 = pd.read_excel(url_18, sep="\t", sheet_name=sheet_name)
+df_18.head()
+
+# Save locally as csv file for faster read or in case url goes bad.
+df_18.to_csv("data_18.csv")
 
 #%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
